35c62ae76d4d98688ac04cab04d393ff1e13d680
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-01-31 00:49:11 +0100

    simple json reader for initial testing

--- util/file_manager.py
+++ util/file_manager.py
@@ -1,2 +1,21 @@
-class FileManager:
-    pass
+import json
+import pickle
+from typing import Dict, List, Union, TypeVar, Optional
+
+
+ReadType = Dict[str, List[Dict[str, Union[int, float, str, List[str]]]]]
+
+# TODO Generalise ReadJSON
+
+
+def ReadJSON(path) -> Union[ReadType, None]:
+    mData: Optional[ReadType] = None
+    with open(path, "r") as mFile:
+        mData = json.load(mFile)
+        mFile.close()
+    return mData
+
+
+
+
+
