1a0e1c5ec0e329cfa3f7dc276d7ae56639adf136
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-02-02 21:32:51 +0100

    getters and dunders

--- game/supply.py
+++ game/supply.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from util.constants import SUPPLY, DEMAND
 
 
@@ -13,3 +14,25 @@ class Supply:
         self.__value: int = value
         self.__weight: int = weight
         self.__typeof: str = typeof
+
+    @property
+    def name(self) -> str:
+        return self.__name.replace("_", " ")
+
+    @property
+    def value(self) -> int:
+        return self.__value
+
+    @property
+    def weight(self) -> int:
+        return self.__weight
+
+    @property
+    def typeof(self) -> str:
+        return self.__typeof
+
+    def __eq__(self, otherSupply: Supply) -> bool:
+        return self.name == otherSupply.name
+
+    def __repr__(self) -> str:
+        return f"[ {self.name.upper()}, {self.value}G, {self.weight}T ]  "
