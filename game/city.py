88e778c5b5805bc575b6c1c541e51943b49f779c
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-02-02 21:30:28 +0100

    adding start city list as well as dunder methods

--- game/city.py
+++ game/city.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
-from typing import Tuple, List, Dict, Union
+from typing import Tuple, List, Dict, Union, TypeVar
 
 from game.supply import Supply
+from util.functions import DistanceBetween
 from util.constants import SUPPLY, DEMAND, CITIES, SUPPLIES
+
+# TODO Introduce a city amount of supplies/demands which corresponds to the size of the city
+
+Player = TypeVar("Player")
+START_CITIES: List[City] = []
 
 
 class City:
@@ -28,11 +34,15 @@ class City:
 
     @property
     def sName(self) -> str:
-        return self.__name[:3]
+        return self.__name[:3].upper()
 
     @property
     def coordinates(self) -> Tuple[float, float]:
         return self.__phi, self.__gamma
+
+    @property
+    def size(self) -> int:
+        return self.__size
 
     @property
     def supplies(self) -> List[Supply]:
@@ -42,11 +52,24 @@ class City:
     def demands(self) -> List[Supply]:
         return self.__demands
 
-    def IsStartCity(self) -> bool:
-        return self.__size <= 1
+    def DistanceStr(self, otherCity: City, player: Player) -> str:
+        distance: int = DistanceBetween(self, otherCity)
+        return f"""
+NAME    : {self.name.upper()}
+DISTANCE: {distance}G
+COST    : {player.CalculateDistanceCost(distance)}KM
+SUPPLY  : {"".join([i.__repr__() for i in self.supplies])}
+DEMAND  : {"".join([i.__repr__() for i in self.demands])}
+KEY     : {self.sName.upper()}
+"""
+
+    @staticmethod
+    def GetStartCities() -> List[City]:
+        return START_CITIES
 
     @staticmethod
     def GenerateCityList() -> List[City]:
+        global START_CITIES
         mCities: List[City] = []
         city: Dict[str, Union[int, float, str, List[str]]]
         for city in CITIES:
@@ -66,14 +89,19 @@ class City:
                     DEMAND
                 ) for i in range(len(SUPPLIES)) if SUPPLIES[i]["name"] in city["demand"]
             ]
-            mCities.append(
-                City(
-                    city["name"],
-                    city["phi"],
-                    city["gamma"],
-                    city["size"],
-                    mSupplies,
-                    mDemands
-                )
-            )
+            mCity = City(city["name"], city["phi"], city["gamma"], city["size"], mSupplies, mDemands)
+            if int(city["size"]) == 1:
+                START_CITIES.append(mCity)
+            mCities.append(mCity)
         return mCities
+
+    def __eq__(self, otherCity: City) -> bool:
+        return self.name == otherCity.name
+
+    def __repr__(self) -> str:
+        return f"""
+NAME  : {self.name.upper()}
+SUPPLY: {"".join([i.__repr__() for i in self.supplies])}
+DEMAND: {"".join([i.__repr__() for i in self.demands])}
+KEY   : {self.sName.upper()}
+"""
