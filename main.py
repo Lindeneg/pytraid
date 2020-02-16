673048b2c23cf47d607b4d249cad5f302a799d5c
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-02-16 06:07:32 +0100

    initiate a new game

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
+"""
+Author : Christian Lindeneg
+         https://github.com/Lindeneg
+Contact: christian@lindeneg.org
+Licence: Public Domain
+"""
+
 from game.game import Game
-from player.player import Player
-from city.city import City
-from train.train import Train
-
-cities = City.GenerateCityList()
-trains = Train.GenerateTrainsDict()
-startCities = City.GetStartCities()
-
-
-player = Player("Christian", startCities[1])
-player2 = Player("Far", startCities[0])
-players = [player, player2]
 
 
 def main():
-    game = Game(players, cities, trains)
+    game: Game = Game.Setup()
     game.StartGame()
 
 
