0824cd85d9fcc9bf88d9583ff611ae8e47471e81
wh473v3rw0rk5 <christianlindeneg@gmail.com>
2020-02-16 04:48:04 +0100

    initating game

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from game.game import Game
+from player.player import Player
+from city.city import City
+from train.train import Train
+
+cities = City.GenerateCityList()
+trains = Train.GenerateTrainsDict()
+startCities = City.GetStartCities()
+
+
+player = Player("Christian", startCities[1])
+player2 = Player("Far", startCities[0])
+players = [player, player2]
 
 
 def main():
-    Game()
+    game = Game(players, cities, trains)
+    game.StartGame()
 
 
 if __name__ == "__main__":
